e8934c981f29a5a9b124ab7fa62e08b93e9c71bc
Benoît Legat <benoit.legat@gmail.com>
2023-10-13 16:19:50 +0200

    Add Manifold opt for cvp

--- src/cvp.jl
+++ src/cvp.jl
@@ -1,4 +1,5 @@
 import ManifoldsBase
+import Manifolds
 import Manopt
 export eliminate_indices
 
@@ -13,33 +14,17 @@ struct Ellipsoid{T,M<:AbstractMatrix{T}} <: ManifoldsBase.AbstractManifold{Manif
     A::M
 end
 
-function ManifoldsBase.retract!(
-    M::Ellipsoid,
-    q,
-    p,
-    x,
-    t::Number,
-    _::Manopt.AbstractRetractionMethod,
-)
+ManifoldsBase.manifold_dimension(e::Ellipsoid) = size(e.A, 2)
+
+ManifoldsBase.inner(::Ellipsoid, p, x, y) = LinearAlgebra.dot(x, y)
+
+# See Example 3.49, p. 40 of Boumal's book for the retraction on the Sphere
+function ManifoldsBase.retract_project!(M::Ellipsoid, q, p, x, t::Number)
     return ManifoldsBase.project!(M, q, p + t * x)
 end
 
-# Not sure if that is correct
-function ManifoldsBase.norm(
-    ::Ellipsoid,
-    p,
-    x,
-)
-    return sqrt(x'x)
-end
-
 # Projection of `y` to the tangent space at `x`
-function ManifoldsBase.project!(
-    M::Ellipsoid,
-    ret,
-    x,
-    y,
-)
+function ManifoldsBase.project!(M::Ellipsoid, ret, x, y)
     z = M.A' * (M.A * x)
     ret .= y - z * (z'y) / (z'z)
     @assert isapprox(ret' * z, 0, atol = 1e-8)
@@ -47,56 +32,146 @@ function ManifoldsBase.project!(
 end
 
 # Projection of `x` to the manifold
-function ManifoldsBase.project!(
-    M::Ellipsoid,
-    ret,
-    x,
-)
+function ManifoldsBase.project!(M::Ellipsoid, ret, x)
     ret .= x ./ sqrt(x' * M.A' * M.A * x)
     @assert ret' * M.A' * M.A * ret ≈ 1
     return ret
 end
 
-function ManifoldsBase.zero_vector!(
-    ::Ellipsoid,
-    z,
-    ::AbstractVector,
-)
+function ManifoldsBase.zero_vector!(::Ellipsoid, z, ::AbstractVector)
     z .= 0
     return z
+end
+
+import ForwardDiff
+
+function _eliminate_indices(
+    manifold::ManifoldsBase.AbstractManifold,
+    A::AbstractMatrix,
+    I::AbstractVector{<:Integer};
+    partition,
+    start = rand(size(A, 1)),
+    solver = Manopt.GradientDescentState,
+    kws...,
+)
+    AI = A[:, I]
+    if solver === Manopt.LevenbergMarquardt
+        function f(M, x)
+            return AI' * x
+        end
+        function jacobian_f(M, p; basis_domain)
+            #N = LinearAlgebra.nullspace(p.x[1]')
+            #B = Matrix{Float64}(undef, size(AI, 1) - 1, size(AI, 2))
+            #n = length(p.x[1])
+            #B[1:(n-1), :] = N' * AI[1:n, :]
+            #B[n:end, :] = AI[(n+1):end, :]
+            #return B'
+            X0 = zeros(ManifoldsBase.manifold_dimension(M))
+            J = ForwardDiff.jacobian(
+                x -> f(
+                    M,
+                    ManifoldsBase.exp(
+                        M,
+                        p,
+                        ManifoldsBase.get_vector(M, p, x, basis_domain),
+                    ),
+                ),
+                X0,
+            )
+            return J
+        end
+        x = Manopt.LevenbergMarquardt(manifold, f, jacobian_f, partition(start), length(I))
+    else
+        function eval_f_cb(M, x)
+            val = LinearAlgebra.norm(AI' * x)^2 / 2
+            return val
+        end
+        function eval_grad_f_cb(M, x)
+            g = partition(AI * (AI' * x))
+            grad = ManifoldsBase.project(M, x, g)
+            return grad
+        end
+        mgo = Manopt.ManifoldGradientObjective(eval_f_cb, eval_grad_f_cb)
+        dmgo = Manopt.decorate_objective!(manifold, mgo)
+        problem = Manopt.DefaultManoptProblem(manifold, dmgo)
+        s = solver(manifold, partition(start); kws...)
+        state = Manopt.decorate_state!(s)
+        Manopt.solve!(problem, state)
+        obj = Manopt.get_objective(problem)
+        x = Manopt.get_solver_return(obj, state)
+    end
+    return A' * x
 end
 
 function eliminate_indices(
     A::AbstractMatrix,
     I::AbstractVector{<:Integer};
-    descent_state_type = Manopt.GradientDescentState,
-    start = rand(size(A, 1)),
     stepsize = Manopt.ConstantStepsize(0.1),
+    retraction_method = ManifoldsBase.ProjectionRetraction(),
     kws...,
 )
     J = setdiff(axes(A, 2), I)
-    AI = A[:, I]
-    AJ = A[:, J]
-    function eval_f_cb(M, x)
-        val = norm(AI' * x)^2 / 2
-        return val
-    end
-    function eval_grad_f_cb(M, x)
-        grad = ManifoldsBase.project(M, x, AI * (AI' * x))
-        return grad
-    end
-    manifold = Ellipsoid(AJ')
-    mgo = Manopt.ManifoldGradientObjective(eval_f_cb, eval_grad_f_cb)
-    dmgo = Manopt.decorate_objective!(manifold, mgo)
-    problem = Manopt.DefaultManoptProblem(manifold, dmgo)
-    s = descent_state_type(manifold, start; stepsize, kws...)
-    state = Manopt.decorate_state!(s)
-    Manopt.solve!(problem, state)
-    obj = Manopt.get_objective(problem)
-    return A' * Manopt.get_solver_return(obj, state)
+    manifold = Ellipsoid(A[:, J]')
+    return _eliminate_indices(
+        manifold,
+        A,
+        I;
+        stepsize,
+        partition = identity,
+        retraction_method,
+        kws...,
+    )
 end
 
-function cvp(p, maxdegree; kws...)
+function eliminate_indices(
+    A::AbstractMatrix,
+    I::AbstractVector{<:Integer},
+    num_nz_rows::Integer;
+    kws...,
+)
+    num_z_rows = size(A, 1) - num_nz_rows
+    partition(x) = Manifolds.ArrayPartition(x[1:num_nz_rows], x[(num_nz_rows+1):end])
+    manifold = Manifolds.ProductManifold(
+        Manifolds.Sphere(num_nz_rows - 1),
+        Manifolds.Euclidean(num_z_rows),
+    )
+    return _eliminate_indices(manifold, A, I; partition, kws...)
+end
+
+function pure_power(mono, var)
+    return MP.degree(mono) == MP.degree(mono, var)
+end
+
+function _zero_constant(p)
+    return iszero(MP.coefficient(p, MP.constant_monomial(p)))
+end
+
+function pure_σ_rows(M::MacaulayMatrix, i::Int, σ::MP.AbstractVariable)
+    σ_rows = Int[]
+    other_rows = Int[]
+    row = 0
+    for (shift, statuses) in zip(M.row_shifts.monomials, M.shift_statuses)
+        for (j, status) in enumerate(statuses)
+            if status == INCLUDED || status == NOT_REDUNDANT
+                row += 1
+                if pure_power(shift, σ) && (j == i || !_zero_constant(M.polynomials[j]))
+                    push!(σ_rows, row)
+                else
+                    push!(other_rows, row)
+                end
+            end
+        end
+    end
+    return σ_rows, other_rows
+end
+
+function cvp(
+    p,
+    maxdegree;
+    ellipsoid = false,
+    rank_check = MM.LeadingRelativeRankTol(Base.rtoldefault(Float64)),
+    kws...,
+)
     σ = MP.similar_variable(p, :σ)
     vars = MP.variables(p)
     grad = MP.differentiate(p, vars)
@@ -105,9 +180,33 @@ function cvp(p, maxdegree; kws...)
     N = eachindex(M.column_basis.monomials)
     non_pure_σ = findall(N) do i
         mono = M.column_basis.monomials[i]
-        MP.degree(mono) != MP.degree(mono, σ)
+        return !pure_power(mono, σ)
     end
     J = setdiff(N, non_pure_σ)
-    v = eliminate_indices(Matrix(SparseArrays.sparse(M)), non_pure_σ)
-    return MP.polynomial(v[J], M.column_basis.monomials[J]), LinearAlgebra.norm(v[non_pure_σ])
+    S = SparseArrays.sparse(M)
+    if ellipsoid
+        v = eliminate_indices(S, non_pure_σ; kws...)
+    else
+        σ_rows, other_rows = pure_σ_rows(M, 1, σ)
+        reordered = [σ_rows; other_rows]
+        S = S[reordered, :]
+        n = length(σ_rows)
+        # We now work in the manifold `Sphere(n-1) × R^(m-n)`
+        if !isnothing(rank_check)
+            A = S[1:n, J]
+            F = LinearAlgebra.svd(Matrix(A), full = true)
+            r = MM.rank_from_singular_values(F.S, rank_check)
+            # We reformulate `x` as `U * y`.
+            # Since `A' * U[:, (r+1):end] * y = 0`, we can reformulate as
+            # `Sphere(r-1) × R^(m-r)`
+            # TODO this destroys sparsity, we shouldn't actually do this product
+            S = [F.U' * S[1:n, :]; S[(n+1):end, :]]
+        else
+            r = n
+            F = nothing
+        end
+        v = eliminate_indices(S, non_pure_σ, r; kws...)
+    end
+    return MP.polynomial(v[J], M.column_basis.monomials[J]),
+    LinearAlgebra.norm(v[non_pure_σ])
 end
--- test/cvp.jl
+++ test/cvp.jl
@@ -3,31 +3,64 @@ module TestCVP
 using LinearAlgebra
 using Test
 import Macaulay
+using Random
 
 function test_elim(T = Float64)
+    Random.seed!(0)
     M = T[
         1 0 1
         0 1 1
     ]
     x = Macaulay.eliminate_indices(M, [3])
-    @test x / x[1] ≈ [1, -1, 0]
+    @test x / x[1] ≈ [1, -1, 0] rtol = 1e-6
+    x = Macaulay.eliminate_indices(M, [3], 2)
+    @test x / x[1] ≈ [1, -1, 0] rtol = 1e-6
+    x = Macaulay.eliminate_indices(M, [3], 2, solver = Macaulay.Manopt.LevenbergMarquardt)
+    @test x / x[1] ≈ [1, -1, 0] rtol = 1e-6
+    M = T[
+        1 0 1 0
+        0 1 0 1
+        0 0 1 -1
+    ]
+    x = Macaulay.eliminate_indices(M, [3, 4], 2; solver = Macaulay.Manopt.LevenbergMarquardt)
+    @test x / x[1] ≈ [1, -1, 0, 0] rtol = 1e-6
 end
 
 using TypedPolynomials
 import MultivariatePolynomials as MP
 
 function test_cvp(T = Float64)
+    Random.seed!(0)
     @polyvar x
-    for α in [zero(T), one(T), T(2)]
-        p = x^2 - 2x + α
-        c, ε = Macaulay.cvp(p, 2)
-        @test MP.maxdegree(c) == 1
-        c /= MP.leading_coefficient(c)
-        vars = MP.effective_variables(c)
-        @test length(vars) == 1
-        σ = first(vars)
-        @test c ≈ one(T) * σ - α + one(T) atol=1e-3
-        @test ε < 1e-3
+    @testset "$ellipsoid" for ellipsoid in [true, false]
+        solvers = Any[]
+        if ellipsoid
+            # Failing on ci for 32-bits with non-ellipsoid
+            push!(solvers, Macaulay.Manopt.GradientDescentState)
+        else
+            # Missing https://github.com/JuliaManifolds/Manifolds.jl/blob/106a8f35f25c5e0501b7b9ec7cf6456e4effe947/src/manifolds/Sphere.jl#L254-L265
+            # for ellispoid to work with LevenbergMarquardt
+            push!(solvers, Macaulay.Manopt.LevenbergMarquardt)
+        end
+        rank_checks = Any[nothing]
+        if !ellipsoid
+            push!(rank_checks, Macaulay.MM.LeadingRelativeRankTol(Base.rtoldefault(Float64)))
+        end
+        @testset "$(string(solver)[1:8])" for solver in solvers
+            @testset "$(string(rank_check)[1:4])" for rank_check in rank_checks
+                @testset "$α" for α in [zero(T), one(T), T(2)]
+                    p = x^2 - 2x + α
+                    c, ε = Macaulay.cvp(p, 2; ellipsoid, solver, rank_check)
+                    @test MP.maxdegree(c) == 1
+                    c /= MP.leading_coefficient(c)
+                    vars = MP.effective_variables(c)
+                    @test length(vars) == 1
+                    σ = first(vars)
+                    @test c ≈ one(T) * σ - α + one(T) atol=1e-3
+                    @test ε < 1e-3
+                end
+            end
+        end
     end
 end
 
